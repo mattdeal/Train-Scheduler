f81246ccd40e6640bcbcf2901fa37843173ad96f
mwdeal1 <mwdeal1@gmail.com>
2017-01-28 09:28:49 -0500

    initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,22 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyBeOtSzAzlB4jpdX2AFH01ZAkAgTdD3K60",
+	authDomain: "trains-97d98.firebaseapp.com",
+	databaseURL: "https://trains-97d98.firebaseio.com",
+	storageBucket: "trains-97d98.appspot.com",
+	messagingSenderId: "980557471358"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+$(document).ready(function() {
+
+// todo: stuff
+
+});
+
+//todo: function to get changes to db
+
+//todo: function to add train to db
